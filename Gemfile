3e5a9a923c211aac6e5b030dd68be85d9b95c90b
SHERRY <swasieko@gmail.com>
2017-09-23 02:02:34 +0000

    Integrated Devise

--- Gemfile
+++ Gemfile
@@ -35,9 +35,14 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails', '~> 3.6'
+  gem 'capybara'
+  gem 'database_cleaner'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+ describe 'creation' do
+   before do
+     @user = User.create(email: "test@test.com", username: "Snowball", first_name: "John", last_name: "Snow", password: "password", password_confirmation: "password")
+   end
+   
+   it 'should be able to be created if valid' do
+     expect(@user).to be_valid
+   end
+   
+   it 'should have a default role of: student' do
+     expect(@user.role).to eq('student')
+   end
+   
+   describe 'validations' do
+     it 'should not be valid without a first name' do
+       @user.first_name = nil
+       expect(@user).to_not be_valid
+     end
+     
+     it 'should not be valid without a last name' do
+       @user.last_name = nil
+       expect(@user).to_not be_valid
+     end
+     
+     it 'should not be valid without a username' do
+       @user.username = nil
+       expect(@user).to_not be_valid
+     end
+     
+     it 'should ensure that all usernames are unique' do
+      duplicate_username_user = User.create(email: "test2@test.com", password: "password", password_confirmation: "password", first_name: "Joni", last_name: "Snowy", username: "watcheronthewall")
+      expect(duplicate_username_user).to be_valid
+      end
+      
+      it 'should ensure that usernames do not allow special characters' do
+        @user.username = "*#*(@!"
+        expect(@user).to_not be_valid
+      end
+     
+  
+   end
+ end
+end
